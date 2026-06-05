6758bfe9bd9ef9be48a80ebeb9e7c23d0f96452b
Milko Roine <milko.roine@gmail.com>
2026-06-05 21:15:32 +0300

    first commit

---
+++ Makefile
@@ -0,0 +1,29 @@
+APP := demoscene
+SRC := src/main.cpp
+BIN_DIR := build
+OUT := $(BIN_DIR)/$(APP)
+
+CXX := g++
+CXXFLAGS := -std=c++17 -O2 -Wall -Wextra
+RAYLIB_FLAGS := $(shell pkg-config --cflags --libs raylib 2>/dev/null)
+ifeq ($(strip $(RAYLIB_FLAGS)),)
+RAYLIB_FLAGS := -I/usr/local/include -L/usr/local/lib -lraylib
+endif
+PLATFORM_LIBS := -lm -lpthread -ldl -lrt -lX11
+
+.PHONY: all setup run clean
+
+all: $(OUT)
+
+setup:
+	./getting_started.sh
+
+$(OUT): $(SRC)
+	mkdir -p $(BIN_DIR)
+	$(CXX) $(CXXFLAGS) $< -o $@ $(RAYLIB_FLAGS) $(PLATFORM_LIBS)
+
+run: $(OUT)
+	./$(OUT)
+
+clean:
+	rm -rf $(BIN_DIR)
---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include "raylib.h"
+
+int main() {
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
+
+    InitWindow(screenWidth, screenHeight, "raylib demoscene starter");
+
+    Camera3D camera = {0};
+    camera.position = (Vector3){50.0f, 0.0f, -35.0f};
+    camera.target = (Vector3){50.0f, 5.0f, 0.0f};
+    camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    const Vector3 dotPosition = {50.0f, 5.0f, 0.0f};
+    const float dotRadius = 0.12f;
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        UpdateCamera(&camera, CAMERA_ORBITAL);
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        BeginMode3D(camera);
+        DrawSphere(dotPosition, dotRadius, RAYWHITE);
+        DrawGrid(20, 1.0f);
+        EndMode3D();
+
+        DrawText("Raylib is working!!", 20, 20, 24, LIGHTGRAY);
+        DrawText("Wubbalubbadubdub", 20, 52, 18, GRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
