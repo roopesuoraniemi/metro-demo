cbb5241816f3e597b25d32f413300eafe93185ae
Roosa <roosasahri1@gmail.com>
2026-06-06 11:37:22 +0300

    kamaa

--- src/main.cpp
+++ src/main.cpp
@@ -19,25 +19,47 @@ int main() {
     const Vector3 dotPosition = {50.0f, 5.0f, 0.0f};
     const float dotRadius = 0.12f;
 
+    RenderTexture2D target = LoadRenderTexture(screenWidth, screenHeight);
+    Shader bloomShader = LoadShader(0, "bloom.fs");
+
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         UpdateCamera(&camera, CAMERA_ORBITAL);
 
+        BeginTextureMode(target);
+          ClearBackground(BLACK);
+
+          BeginMode3D(camera);
+              DrawSphere(dotPosition, dotRadius, RAYWHITE);
+              DrawFinnishMetro();
+              DrawGrid(20, 1.0f);
+          EndMode3D();
+        EndTextureMode();
+
         BeginDrawing();
-        ClearBackground(BLACK);
+            ClearBackground(BLACK);
 
-        BeginMode3D(camera);
-        DrawSphere(dotPosition, dotRadius, RAYWHITE);
-        DrawFinnishMetro();
-        DrawGrid(20, 1.0f);
-        EndMode3D();
+            BeginShaderMode(bloomShader);
 
-        DrawText("Raylib is working!!", 20, 20, 24, LIGHTGRAY);
-        DrawText("Wubbalubbadubdub", 20, 52, 18, GRAY);
+                DrawTextureRec(
+                    target.texture,
+                    (Rectangle){ 0, 0, (float)target.texture.width, (float)-target.texture.height },
+                    (Vector2){ 0, 0 },
+                    WHITE
+                );
+            EndShaderMode();
+
+            DrawText("Raylib is working!!", 20, 20, 24, LIGHTGRAY);
+            DrawText("Wubbalubbadubdub", 20, 52, 18, GRAY);
 
         EndDrawing();
     }
+
+    UnloadShader(bloomShader);
+    UnloadRenderTexture(target);
+    CloseWindow();
 
     CloseWindow();
     return 0;
